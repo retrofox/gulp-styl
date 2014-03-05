ac57c29e672f9aa99a46e8d2240168bedbe6f473
Damian Suarez <rdsuarez@gmail.com>
2014-03-05 10:06:12 -0300

    example: add gulp file

---
+++ example/gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * Module dependencies
+ */
+
+var gulp = require('gulp');
+var styl = require('../');
+
+/**
+ * Compile css
+ */
+
+gulp.task('css', function () {
+  gulp.src('./*.styl')
+    .pipe(styl({
+      whitespace: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+// Default
+gulp.task('default', ['css']);
